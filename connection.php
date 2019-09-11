39bdfc00cf7acde157d687e8c1188e280ae1fd67
nuelchi1 <54984583+nuelchi1@users.noreply.github.com>
2019-09-11 18:53:39 +0100

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+	//Establish Database connection
+	$server = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "result";
+	
+	$connection = mysqli_connect($server, $username, $password, $dbname);
+	
+	if(!$connection){
+		die("Awaiting Resources");
+	}
+?>
